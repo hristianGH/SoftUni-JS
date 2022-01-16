87735c861251a1ff615b6a24caf3f9ad38cd0cb3
hristianGH <hristovhristian7@gmail.com>
2022-01-16 20:13:25 +0200

    Statements

---
+++ Basics/3.ConditionalStatements/1.ExelentResult.js
@@ -0,0 +1,8 @@
+function Result(grade){
+    if(grade>=5.50){
+        console.log('Excellent!')
+    }
+    else {
+        console.log()
+    }
+}
---
+++ Basics/3.ConditionalStatements/2.BiggerNum.js
@@ -0,0 +1,7 @@
+function Comparer(num1,num2)
+{
+    if(num1>num2){
+        console.log(num1);
+    }
+    else{console.log(num2)}
+}
