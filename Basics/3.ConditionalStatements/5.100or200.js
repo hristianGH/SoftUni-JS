423c62bdf468198de5291755edb50978ef5de0b5
hristianGH <hristovhristian7@gmail.com>
2022-01-16 20:35:51 +0200

    6.Speed

---
+++ Basics/3.ConditionalStatements/5.100or200.js
@@ -0,0 +1,13 @@
+function LessOrMore(num1){
+if(num1<100)
+{
+    console.log('Less than 100')
+}
+else if(num1>100&&num1<200)
+{
+    console.log('Between 100 and 200')
+}
+else{
+    console.log('Greater than 200')
+}
+}
---
+++ Basics/3.ConditionalStatements/6.Speed.js
@@ -0,0 +1,21 @@
+function Speed(num){
+    if(num<10){
+console.log('slow')
+    }
+    else if(num>10&&num<=50){
+        console.log('average')
+
+    }
+    else if(num>10&&num<=50){
+        console.log('fast')
+
+    }
+    else if(num>10&&num<=50){
+        console.log('ultra fast')
+
+    }
+    else{
+        console.log('extreamly fast')
+
+    }
+}
