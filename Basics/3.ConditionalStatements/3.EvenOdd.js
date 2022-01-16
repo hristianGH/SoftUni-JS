fe3b7978489b74c1e72296fae25596be4d5134c1
hristianGH <hristovhristian7@gmail.com>
2022-01-16 20:29:53 +0200

    4.Password

---
+++ Basics/3.ConditionalStatements/3.EvenOdd.js
@@ -0,0 +1,10 @@
+function EvenOrOdd(num)
+{
+    if(num%2==0)
+    {
+        console.log('even')
+    }
+    else{
+        console.log('odd')
+    }
+}
---
+++ Basics/3.ConditionalStatements/4.Password.js
@@ -0,0 +1,10 @@
+function Guess(pass)
+{
+    if(pass=='s3cr3t!P@ssw0rd')
+    {
+        console.log('Welcome')
+    }
+    else{
+        console.log('Wrong password!')
+    }
+}
