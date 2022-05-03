0aafeacf7548423ed4be0a9385f00d47e8ce1fa4
hristianGH <hristovhristian7@gmail.com>
2022-05-03 13:11:13 +0300

    dropdown and encode/decode

--- Advanced/DOM Events/04. Fill-Dropdown/dropdown.js
+++ Advanced/DOM Events/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+    const textInput= document.getElementById('newItemText');
+    const valueInput = document.getElementById('newItemValue');
+    const button = document.getElementsByTagName('input')[2];
+    button.addEventListener('click',(e)=>{
+        let ele = document.createElement('option');
+        ele.innerHTML=`${textInput.value} ${valueInput.value}`
+        document.getElementById('menu').appendChild(ele);
+        textInput.value=''
+        valueInput.value=''
+    })
 }
--- Advanced/DOM Events/05. Encode-and-Decode-Messages/app.js
+++ Advanced/DOM Events/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,28 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+  const divs = {
+    encode: main.querySelectorAll("div")[0],
+    decode: main.querySelectorAll("div")[1],
+  };
+  const encode = {
+    Text: divs.encode.getElementsByTagName("textarea")[0],
+    encodeBtn: divs.encode.getElementsByTagName("button")[0],
+    func:(char) => String.fromCharCode(char.charCodeAt(0) + 1),
+  };
+  const decode = {
+    Text: divs.decode.getElementsByTagName("textarea")[0],
+    decodeBtn: divs.decode.getElementsByTagName("button")[0],
+    func:(char) => String.fromCharCode(char.charCodeAt(0) - 1),
+  };
+
+  encode.encodeBtn.addEventListener("click", (e) => {
+    let msg = encode.Text.value.split('').map(encode.func).join('');
+    decode.Text.value=msg;
+    encode.Text.value='';
+  });
+
+  decode.decodeBtn.addEventListener("click", (e) => {
+    let msg = decode.Text.value.split('').map(decode.func).join('');
+    encode.Text.value=msg;
+    decode.Text.value='';
+  });
 }
