7f34acf947172ef14dc9d15a3eb09abbcbde8629
hristianGH <hristovhristian7@gmail.com>
2022-05-08 16:21:07 +0300

    exercise

--- Advanced/DOM Events/06. Furniture/solution.js
+++ Advanced/DOM Events/06. Furniture/solution.js
@@ -1,4 +1,28 @@
 function solve() {
+  const textAreas = document.querySelectorAll("textarea");
+  const buttons = document.querySelectorAll("button");
 
-  //TODO...
+  const furnitures = document.querySelectorAll;
+
+  const generate = { text: textAreas[0], button: buttons[0] };
+  const buy = { text: textAreas[1], button: buttons[1] };
+  
+
+  generate.button.addEventListener('click', (e)=>{
+
+  })
 }
+
+/* {
+  "img":"https://www.ikea.com/PIAimages/0447583_PE597395_S5.JPG",
+  "name": "Sofa",
+  "price": "259",
+  "decFactor":"0.4"
+
+},
+{
+  "img":"https://www.stylespafurniture.com/wp-content/uploads/2020/03/Cove_3_Door_Wardrobe_1.jpg",
+  "name": "Wardrobe",
+  "price": "120",
+  "decFactor":"1.2"
+} */
